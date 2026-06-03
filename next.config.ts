f6d8332988d41d9e3fab3e08e7dc2502e601971f
Jimi Roi <rnurazim@gmail.com>
2026-06-03 13:53:23 +0800

    Fix Clerk setup and middleware routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: '/sign-in',
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: '/sign-up',
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_URL: '/',
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_URL: '/',
+  }
 };
 
 export default nextConfig;
