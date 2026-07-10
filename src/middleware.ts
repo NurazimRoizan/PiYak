0cfc9b587d240010d1ab1381f54b70aa3eab2d47
Jimi Roi <rnurazim@gmail.com>
2026-07-10 11:31:31 +0800

    feat: Overhaul PWA by removing custom Service Worker caching to fix Clerk auth bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
-export default clerkMiddleware()
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and static files
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for Clerk's auto-proxy path
     '/__clerk/(.*)',
